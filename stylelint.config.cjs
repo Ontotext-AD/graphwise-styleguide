b05e265af78605406efbd79673adb448a78ca320
svilen.velikov <svilen.velikov@ontotext.com>
2025-09-18 18:39:09 +0300

    Fixes eslint and stylelint configs. Prepare the package for publishing: adding version and described files for publishing.

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,6 +1,11 @@
-const path = require('node:path');
-
 module.exports = {
-  extends: [path.resolve(__dirname, '../../stylelint.config.base.cjs')],
-  ignoreFiles: ['**/node_modules/**', 'dist/**'],
+  root: true,
+  plugins: ["stylelint-scss"],
+  extends: ["stylelint-config-standard-scss"],
+  rules: {
+    "max-nesting-depth": 3,
+    "no-descending-specificity": true,
+    "string-quotes": "single"
+  },
+  ignoreFiles: ["**/node_modules/**", "dist/**"],
 };
